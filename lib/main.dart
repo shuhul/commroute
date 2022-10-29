91f1cbdfef4cc22cc519768dab4d3a0cf9217462
shuhul <shuhulmujoo@gmail.com>
2022-10-29 16:08:43 -0700

    v0.0.2 - Created Log In Screen and made helper methods

--- lib/main.dart
+++ lib/main.dart
@@ -19,62 +19,70 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Center(child: Text('Commroute')),),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: ElevatedButton(
-                child: const Text('Log in'),
-                onPressed: () {
-
-                }
-              )
-            )
-            ,
-            ElevatedButton(
-              child: const Text('Create an Account'),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const CreateAccountScreen()),
-                );
-              },
-            ),
-          ],
-        ),
-      ),
-    );
-  }
+Padding getForm(text){
+  return Padding(
+    padding: const EdgeInsets.symmetric(horizontal: 72, vertical: 16),
+    child: TextFormField( decoration: InputDecoration( border: const UnderlineInputBorder(), labelText: text,)),
+  );
 }
 
+Padding getButton(context, text, widget){
+  return Padding(padding: const EdgeInsets.all(16), child: ElevatedButton(
+    onPressed: () {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => widget),
+      );
+    },
+    child: Text(text),
+  ));
+}
+
+Scaffold getScaffold(titleText, widgets){
+  return Scaffold(
+    appBar: AppBar(title: Center(child: Text(titleText)),),
+    body: Center( child: Column( mainAxisAlignment: MainAxisAlignment.center, children: widgets)),
+  );
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return getScaffold('Commroute', [
+        getButton(context, 'Login To Existing Account', const LoginScreen()),
+        getButton(context, 'Create An Account', const CreateAccountScreen())
+    ]);
+  }
+}
 
 class CreateAccountScreen extends StatelessWidget {
   const CreateAccountScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Center(child: Text('Create Account')),),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => const HomeScreen()),
-            );
-          },
-          child: const Text('Done'),
-        ),
-      ),
-    );
+    return getScaffold('Create Your Account', [
+      getForm('Full Name'),
+      getForm('Grade'),
+      getForm('Email'),
+      getForm('Phone Number'),
+      getForm('Home Address'),
+      getForm('Password'),
+      getButton(context, 'Done', const HomeScreen())
+    ]);
+  }
+}
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return getScaffold('Enter Your Name And Password', [
+      getForm('First Name'),
+      getForm('Password'),
+      getButton(context, 'Log In', const HomeScreen())
+    ]);
   }
 }
